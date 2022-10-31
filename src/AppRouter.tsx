302e31658217fc043a0958fa756eda28e3dc4176
jackhfeinstein <jackhfeinstein@gmail.com>
2022-10-31 09:33:45 -0400

    introducing action to automatically deploy to pages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,32 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, RouteProps, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { HomePage } from './pages/home'
-import { NotFoundPage } from './pages/not-found'
-
-interface Routes {
-  '/': Record<string, unknown>
-}
-
-type RoutePath = keyof Routes
-type Params<TPath extends RoutePath> = TPath extends RoutePath ? Routes[TPath] : never
-
-interface CustomRouteProps<TPath extends RoutePath> extends Omit<RouteProps, 'component' | 'path'> {
-  component: React.ComponentType<Params<TPath>>
-  path: TPath
-}
-
-function AppRoute<TPath extends RoutePath>({ component: Component, ...routeProps }: CustomRouteProps<TPath>) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  return <Route {...routeProps} render={({ match: { params } }) => <Component {...params} />} />
-}
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <AppRoute exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
+        <Route component={HomePage} />
       </Switch>
     </BrowserRouter>
   )
