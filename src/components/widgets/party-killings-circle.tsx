b6554e2bb7006f94b57b62055e10538d814518de
Dmitry Lukyanov <dlukyan@clemson.edu>
2022-11-14 16:12:27 -0500

    fix

--- src/components/widgets/party-killings-circle.tsx
+++ src/components/widgets/party-killings-circle.tsx
@@ -3,7 +3,7 @@ import rawData from '../../data/data_cleansed.json'
 
 import React, { useEffect, useRef } from 'react'
 import { createUseStyles } from 'react-jss'
-import { theme, Theme } from '../../theme'
+import { Theme } from '../../theme'
 import { PoliceViolenceDataPoint } from '../../types/police-violence'
 
 const useStyles = createUseStyles((theme: Theme) => ({
@@ -27,22 +27,22 @@ const useStyles = createUseStyles((theme: Theme) => ({
   info: {
     ...theme.typography.small,
     color: theme.colors.darkGray,
-  }
+  },
 }))
 
 export const PartyKillingsCircle: React.FC = () => {
-  const classes = useStyles();
-  const ref = useRef(null);
-  const data: PoliceViolenceDataPoint[] = rawData as PoliceViolenceDataPoint[];
+  const classes = useStyles()
+  const ref = useRef(null)
+  const data: PoliceViolenceDataPoint[] = rawData as PoliceViolenceDataPoint[]
 
   const dimensions = {
     height: window.innerHeight / 5 - 20,
     width: window.innerWidth / 4 - 20,
-  };
+  }
 
   const pData = {
     Democrats: (data.filter(d => d.congressperson_party === 'Democrat').length / data.length) * 100,
-    Republicans: 100 - (data.filter(d => d.congressperson_party === 'Democrat').length / data.length) * 100
+    Republicans: 100 - (data.filter(d => d.congressperson_party === 'Democrat').length / data.length) * 100,
   }
 
   useEffect(() => {
@@ -51,39 +51,30 @@ export const PartyKillingsCircle: React.FC = () => {
       .attr('width', dimensions.width - 100)
       .attr('height', dimensions.height)
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const y = d3.scaleBand().range([0, 125]).domain(d3.range(10))
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const x = d3.scaleBand().range([0, 125]).domain(d3.range(10))
-
     const data = d3.range(100)
 
-    const color = d3.scaleOrdinal()
-      .range(["blue", "red"])
+    const color = d3.scaleOrdinal().range(['blue', 'red'])
 
-    const pie = d3.pie()
-      .value(function(d) {return d[1]})
+    const pie = d3.pie().value(function (d) {
+      return d[1]
+    })
     const data_ready = pie(Object.entries(pData))
 
-    const container = svgElement.append('g')
-      .attr('transform', 'translate(80, 80)')
+    const container = svgElement.append('g').attr('transform', 'translate(80, 80)')
 
-    const arcGenerator = d3.arc()
-      .innerRadius(0)
-      .outerRadius(78)
+    const arcGenerator = d3.arc().innerRadius(0).outerRadius(78)
 
     container
       .selectAll('mySlices')
       .data(data_ready)
       .join('path')
       .attr('d', arcGenerator)
-      .attr('fill', function(d){ return(color(d.data[1])) })
+      .attr('fill', function (d) {
+        return color(d.data[1])
+      })
       // .attr("stroke", "black")
       // .style("stroke-width", "0px")
-      .style("opacity", 0.7)
+      .style('opacity', 0.7)
       .transition()
       .duration(1500)
 
@@ -92,11 +83,15 @@ export const PartyKillingsCircle: React.FC = () => {
       .data(data_ready)
       .enter()
       .append('text')
-      .text(function(d){ return d.data[0]})
-      .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
-      .style("text-anchor", "middle")
-      .style("font-size", 12)
-      .style("opacity", 0.7)
+      .text(function (d) {
+        return d.data[0]
+      })
+      .attr('transform', function (d) {
+        return 'translate(' + arcGenerator.centroid(d) + ')'
+      })
+      .style('text-anchor', 'middle')
+      .style('font-size', 12)
+      .style('opacity', 0.7)
   }, [dimensions.height, dimensions.width, pData])
 
   return (
